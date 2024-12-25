ea34c643cb1595fc5e0982e7a0f1c04b7af78b23
prozhou <1249503657@qq.com>
2024-12-25 19:10:58 +0800

    feat:修正springboot版本，并修改启动lombok无法识别问题

--- community-server/src/main/java/com/progzhou/community/CommunityServerApplication.java
+++ community-server/src/main/java/com/progzhou/community/CommunityServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.progzhou.community.mapper.*")
 public class CommunityServerApplication {
 
     public static void main(String[] args) {
